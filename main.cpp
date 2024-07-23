4277cb4915050c85c9ceefabc6e67e7d8e69cfe4
Baima1105 <2330781688@qq.com>
2024-07-23 15:05:07 +0800

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include<iostream>
 #include "hello.h"
+#include<string>
 using namespace std;
 int main(){
-    hello();
+    string s="hello world";
+    for(int i=0;i<s.size();i++){
+        s[i]=toupper(s[i]);
+    }
+    cout<<s<<endl;
     return 0;
 }
